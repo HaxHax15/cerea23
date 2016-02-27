b4964973a2beac03afc34f3702e5efa2706ebdd7
HaxHax15 <hax.hax@mail.ru>
2016-02-28 00:45:29 +0300

    wer

--- cerea2/urls.py
+++ cerea2/urls.py
@@ -18,9 +18,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-        url(r'^grappelli/', include('grappelli.urls')),
+    url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^account/', include("account.urls")),
-
 
 ]
